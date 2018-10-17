3bb58790d7f2450d38e745cebc5c2e534b55cf2a
Rohit Mohan <rohit.mohan@gmail.com>
2018-10-17 15:28:39 -0400

    initial web app with express and ejs

--- main.js
+++ main.js
@@ -2,10 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static(__dirname));
+const books = [
+    "Harry Potter and the Sorcerer's Stone",
+    "Harry Potter and the Chamber of Secrets",
+    "Harry Potter and the Prisoner of Azkaban",
+    "Harry Potter and the Goblet of Fire",
+    "Harry Potter and the Order of the Phoenix",
+    "Harry Potter and the Half-Blood Prince",
+    "Harry Potter and the Deathly Hallows"
+];
+
+app.set("view engine", "ejs");
+app.set("views", `${__dirname}/public/views`);
+
+app.use(express.static(`${__dirname}/public`));
 
 app.get("/", (request, response) => {
-    response.send("index.html");
+    response.render("index");
+});
+
+app.get("/books", (request, response) => {
+    response.render("books", {books: books});
+});
+
+app.get("/books/:id", (request, response) => {
+    response.render("book", {book: books[parseInt(request.params.id)]});
 });
 
 app.listen(process.env.PORT || 8081, () => {
